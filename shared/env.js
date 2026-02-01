5365c2502af7c89c270d5d430f7fef08f18719a6
ankinstructor2026-creator <ank.instructor2026@gmail.com>
2026-02-01 14:35:19 +0900

    Add files via upload

---
+++ shared/env.js
@@ -0,0 +1,13 @@
+// shared/env.js
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBpHlwulq6lnbmBzNm0rEYNahWk7liD3BM",
+  authDomain: "ank-project-77283.firebaseapp.com",
+  projectId: "ank-project-77283",
+  storageBucket: "ank-project-77283.firebasestorage.app",
+  messagingSenderId: "707356972093",
+  appId: "1:707356972093:web:03d20f1c1e5948150f8654",
+};
+
+export const API_BASE =
+  "https://ank-admin-api-986862757498.asia-northeast1.run.app";
